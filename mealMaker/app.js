f6290fb65fe80b826ed2e867e11bade972d4fdf1
Savio Felix Marques <saviohc@gmail.com>
2020-10-27 17:59:41 -0300

    mealMaker

---
+++ mealMaker/app.js
@@ -0,0 +1,26 @@
+const menu = {
+_courses: {
+    appetizers: [],
+    mains: [],
+    desserts: [],
+
+    get appetizers() {
+
+    },
+    set appetizers() {
+        
+    },
+    get mains() {
+
+    },
+    set mains() {
+
+    },
+    get desserts() {
+
+    },
+    set desserts() {
+
+    }
+ }
+};
